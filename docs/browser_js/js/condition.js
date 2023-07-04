98d30f34302a91bb84cb0744421ca8dc0938c5d4
YebinKim98 <dpqls9898@gmail.com>
2023-07-04 17:45:29 +0900

    first commit

---
+++ docs/browser_js/js/condition.js
@@ -0,0 +1,31 @@
+// if() {
+// } else if() {
+// } else if() {
+// }
+
+let first = 1;
+let second = 4;
+
+if(first < 10){
+    console.log("mand")
+} else if(first > second) {
+    console.log('${first} > ${second}');
+} else {
+    console.log('second : ${second}');
+}
+
+//== vs ===
+let third = '4';
+first = 4;
+// third == first
+// true 
+// third === first : 데이터 타입
+// false
+
+//삼항 연산자
+// if () {
+    
+
+// } else {
+
+// }
